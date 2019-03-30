98ec56ae9a28fce83e4fc451f5159ee7723304df
Puja Jha <pujajha5912@gmail.com>
2019-03-30 14:07:00 +0530

    Create middleNode.py

---
+++ Linked_List/middleNode.py
@@ -0,0 +1,31 @@
+#https://leetcode.com/problems/middle-of-the-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode """
+        
+        l=[]
+        l1=[]
+        if head is None:
+            return
+        while head is not None:
+            current=head
+            l.append(current.val)
+            head=head.next
+        mid=len(l)/2
+        l1=l[mid:]
+        
+        cur = dummy = ListNode(0)
+        for e in l1:
+            cur.next = ListNode(e)
+            cur = cur.next
+        return dummy.next
+        
