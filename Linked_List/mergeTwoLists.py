f84410f206b8d678e1787d05326d8aa0b806eaf5
Puja Jha <pujajha5912@gmail.com>
2019-04-15 14:06:42 +0530

    Update mergeTwoLists.py

--- Linked_List/mergeTwoLists.py
+++ Linked_List/mergeTwoLists.py
@@ -7,28 +7,25 @@
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l3=ListNode(0)
+        if l1==None and l2==None:
+            return None
+        l3=ListNode
         current=l3
-        if l1 is None:
-            return l2
-        if l2 is None:
-            return l1
-        while l1 is not None and l2 is not None:
-            if(l1.val<=l2.val):
-                current.next=ListNode(l1.val)
-                l1=l1.next
-            else:
-                current.next=ListNode(l2.val)
-                l2=l2.next
-                
-            current=current.next
-            
-            if l1:
-                current.next=ListNode(l1.val)
-            if l2:
-                current.next=ListNode(l2.val)
+        while l1 and l2:
+            if l1 is not None and l2 is not None:
+                if(l1.val<=l2.val):
+                    current.next=l1
+                    l1=l1.next
+                else:
+                    current.next=l2
+                    l2=l2.next
+                current=current.next
+        if l1:
+            current.next=l1
+        elif l2:
+            current.next=l2
         return l3.next
-
+                   
     
 
         
