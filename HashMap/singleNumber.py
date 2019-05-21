4d413569ccfb470ec8877061232ee7e968fb6392
Puja Jha <pujajha5912@gmail.com>
2019-05-21 10:00:14 +0530

    Create singleNumber.py

---
+++ HashMap/singleNumber.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/single-number/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_table={}
+        for i in nums:
+            try:
+                hash_table.pop(i)
+            except:
+                hash_table[i] = 1
+        return hash_table.popitem()[0]
+
