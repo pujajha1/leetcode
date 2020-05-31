67acbb75c431be707e4300270b8c5736322b5adb
Puja Jha <pujajha5912@gmail.com>
2020-05-31 14:37:07 +0530

    Create LongestCommonPrefix.py

---
+++ Dictionary/LongestCommonPrefix.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/longest-common-prefix/submissions/
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        l=[]
+        k={}
+        for i in strs:
+            for j in range(0,len(i)):
+                l.append(i[:j+1])
+        for i in l:
+            if(l.count(i)==len(strs)):
+                k[i]=len(i)
+        if not bool(k):
+            return ""
+        else:
+            return max(k, key=k.get)
