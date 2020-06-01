251279760c28e99f934179bc58f0d9c2ab18ebf2
Puja Jha <pujajha5912@gmail.com>
2020-06-01 14:48:48 +0530

    removing occurence of a particular element from list

---
+++ removeElements.py
@@ -0,0 +1,5 @@
+#https://leetcode.com/problems/remove-element/
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while val in nums:nums.remove(val)
+        print(nums)
