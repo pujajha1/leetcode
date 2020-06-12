fa8d93f39a9f7c2e9b02d18b46278f790502a572
Puja Jha <pujajha5912@gmail.com>
2020-06-12 12:18:43 +0530

    Update isAnagram.py

--- HashMap/isAnagram.py
+++ HashMap/isAnagram.py
@@ -2,3 +2,16 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s)==Counter(t)
+    
+====================================================
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sorted_s=''.join(sorted(s))
+        sorted_t=''.join(sorted(t))
+        if len(s)!=len(t):
+            return False
+        else:
+            if(sorted_s==sorted_t):
+                return True
+            else:
+                return False
