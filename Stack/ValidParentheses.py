54a164332939ee60352ed61c2b259420810e9e6d
Puja Jha <pujajha5912@gmail.com>
2020-06-17 17:05:23 +0530

    Create ValidParentheses.py

---
+++ Stack/ValidParentheses.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/valid-parentheses/submissions/
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for i in s:
+            if i in ['(','{','[']:
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                curr=stack.pop()
+                if curr=='(' :
+                    if i!=')':
+                        return False
+                if curr=='{':
+                    if i!='}':
+                        return False
+                if curr=='[':
+                    if i!=']':
+                        return False
+        if stack:
+            return False
+        return True
