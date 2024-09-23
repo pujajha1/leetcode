0da4144d24c7a7b34506ec62cd34ff13c0397a6b
Puja Jha <pujajha5912@gmail.com>
2024-09-23 19:16:51 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -6,3 +6,19 @@ class Solution:
         if int(''.join(list(str(x))[::-1])) == x:
             return True
         return False
+
+         # 2nd way
+        strinh = str(x)
+        length= len(strinh) - 1
+        for i in range(0,len(strinh)):
+            j = len(strinh)-(i+1)
+            print(strinh[i], strinh[j])
+            if strinh[i] != strinh[j] :
+                return False
+        return True
+
+        # 3rd Way
+        # strinh = str(x)
+        # if strinh == strinh[::-1]:
+        #     return True
+        # return False
